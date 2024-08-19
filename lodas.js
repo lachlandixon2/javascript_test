5208b4af6ea4f86ff7257af9136c4652d6c868c7
lachlandixon2 <lachlan.dixon2@studytafensw.edu.au>
2024-08-19 12:35:26 +1000

    first commit

---
+++ lodas.js
@@ -0,0 +1,8 @@
+let l = require('lodash');
+
+let a = "this is a test";
+
+let b = l.toUpper(a);
+
+console.log(b);
+
---
+++ script.js
@@ -0,0 +1,11 @@
+
+let emp = {
+    name:"Lachlan", 
+    address:"18",
+    phone:"0451463340"
+    
+}
+
+console.log(emp.name)
+console.log(emp.address)
+console.log(emp.phone)
